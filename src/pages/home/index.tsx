3d7d57d08b04715a989504034440286109a0ad5e
CATARINA KONDOLIZA GARDENIA FAGUNDES DE MARTIN <42332992820@info.local>
2026-04-17 11:39:28 -0300

    :construction: Construindo tela home
    - Finalizando estilização do footer
    
    - Adicionando footer na tela home
    
    - Iniciando tela home, parando na section Destaques

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,41 @@
-import Header from "@/components/header/header"
+import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
+import styles from "./home.module.css";
 
 const Home = () => {
     return (
         <>
-            <Header/>
+            <Header />
+            <main>
+                <section id={styles.banner}>
+                    <h1 className={styles.titulo}>BEM-VINDO AO VH BURGUER</h1>
+                    <img src="../imgs/foto_de_hamburgueres.png" alt="Imagem de três hamburgueres"
+                        id={styles.img_banner} />
+                    <div id={styles.botoes_banner}>
+                        <button id={styles.botao_atendente}>Chamar atendente</button>
+                        <button id={styles.botao_cardapio}>Ver cardápio</button>
+                    </div>
+                </section>
+                <section id={styles.destaque}>
+                    <div id={styles.container_destaque}>
+                        <div id={styles.mais_pedidos}>
+                            <p className={styles.txt_menor}>Os queridinhos da galera</p>
+                            <p className={styles.txt_maior}>MAIS PEDIDOS</p>
+                        </div>
+                        <div id={styles.container_dois}>
+                            <div className={styles.lanches}>
+                                <p className={styles.txt_menor}>Lanches com</p>
+                                <p className={styles.txt_maior}>MUITO BACON</p>
+                            </div>
+                            <div className={styles.lanches}>
+                                <p className={styles.txt_menor}>Se tiver muita fome</p>
+                                <p className={styles.txt_maior}>SUPER COMBOS</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <Footer />
         </>
     );
 }
